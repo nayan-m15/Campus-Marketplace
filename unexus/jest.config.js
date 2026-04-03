1a61855c115f956bff6809a86bc654689f2844e0
Saurav Lall <sauravlall11@gmail.com>
2026-04-03 23:48:24 +0200

    Added babel configurations

--- unexus/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: "jsdom", // for React
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-  transform: {
-    "^.+\\.[jt]sx?$": "babel-jest"
-  },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
-};
