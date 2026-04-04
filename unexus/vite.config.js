ef5f58ea9abc3d0f5c69f7648958c1711ca211e0
Hemesh-P <hparshotam11@gmail.com>
2026-04-04 19:14:46 +0200

    fix vitest config for CI

--- unexus/vite.config.js
+++ unexus/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     environment: 'jsdom',
-    globals: true
+    globals: true,
+    include: ['src/**/*.{test,spec}.{js,jsx}']
   }
 })
