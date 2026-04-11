53fbea5e6d84f40a56182e5b07bb827f77f98e6a
Saurav Lall <sauravlall11@gmail.com>
2026-04-11 23:13:37 +0200

    Added darkmode toggle

--- unexus/src/main.jsx
+++ unexus/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") document.documentElement.classList.add("dark");
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
