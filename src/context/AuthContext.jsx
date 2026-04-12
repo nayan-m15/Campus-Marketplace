25571e45dba4a123678a829a1f961900a2dcfadc
Jai Parbhoo <parbhoojai@gmail.com>
2026-04-12 19:56:51 +0200

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -50,7 +50,7 @@ export function AuthProvider({ children }) {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin,
+        redirectTo: `${window.location.origin}${import.meta.env.BASE_URL}`,
       },
     });
     return { data, error };
