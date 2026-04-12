46c55d23f3c8012a2c0d9c99ff9ce52a03f0c9d7
Jai Parbhoo <parbhoojai@gmail.com>
2026-04-12 19:14:07 +0200

    Google auth fix

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,13 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const siteUrl = import.meta.env.VITE_SITE_URL
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
-
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    redirectTo: siteUrl,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  }
+})
