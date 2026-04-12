f0fa05bca041fdefa4471e06ed86dbd51989db71
Hemesh-P <hparshotam11@gmail.com>
2026-04-12 18:21:47 +0200

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Campus-Marketplace/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
