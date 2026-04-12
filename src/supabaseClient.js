fd37c45612d1374ab5a91308d8cab9d7e8dfd329
Jai Parbhoo <parbhoojai@gmail.com>
2026-04-12 20:05:28 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 const siteUrl = import.meta.env.VITE_SITE_URL
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
